dfc8784db0edbbbab7ee0b7391b6da1eec7d8088
Minh Thong <s3852882@rmit.edu.vn>
2022-09-15 00:24:15 +0700

    make some change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
