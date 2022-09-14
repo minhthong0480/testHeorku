1b0cba8174a4a231412fb3a6a47f32b36015b9be
Minh Thong <73870657+minhthong0480@users.noreply.github.com>
2022-09-15 02:01:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require('path')
 
 //import route
 const authRoute = require("./routes/auth");
